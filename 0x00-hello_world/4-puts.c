a83446278c27b589149bd88a997986ec19279006
junluck <junaindavidsvickerman@gmail.com>
2022-09-08 09:42:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h> 
-
+/*
+ * Storing string in characters array
+ *
+ * using puts to display array of characters
+ */
 int main(void)
 {
-	char myPuts[]="Programming is like building a multilingual puzzle";
+	char myPuts[] = "\"Programming is like building a multilingual puzzle";
 	puts(myPuts);
 
 	return 0;
