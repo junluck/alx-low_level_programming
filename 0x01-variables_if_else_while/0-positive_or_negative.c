ed930698061d75325cf79fa72c66cd6bdfeaf8aa
junluck <junaindavidsvickerman@gmail.com>
2022-09-09 07:55:15 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*main-
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+
+	int n =rand();
+	if (n>0)
+	{
+	printf("%i is positive",n);
+	}
+	else if(n==0)
+	{
+	printf("%i is zero",n)
+	}
+	else if(n<0)
+	{
+	printf("%i is negative",n)
+	}
+
+	return (0)
+}
