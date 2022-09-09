84f0de04aee18af9670478203ebf441b444f88a0
junluck <junaindavidsvickerman@gmail.com>
2022-09-09 04:46:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /*
- * main-
+ * main- prints the size of various data types
  *
- * Description:
+ * Description:storing wach size in a variable and then printing it out
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
-	int charSize=sizeof(char);
-	int intSize=sizeof(int);
-	int longSize=sizeof(long int);
-	int longLongSize=sizeof(long long int);
-	int floatSize=sizeof(float);	
-	printf("Size of a char: %i byte(s)\n",charSize);
-	printf("Size of an int: %i byte(s)\n",intSize);
-	printf("Size of a long int: %i byte(s)\n",longSize);
-	printf("Size of a long long int: %i byte(s)\n",longLongSize);
-	printf("Size of a float: %i byte(s)\n",floatSize);
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longLongSize = sizeof(long long int);
+	int floatSize = sizeof(float);
+	printf("Size of a char: %i byte(s)\n", charSize);
+	printf("Size of an int: %i byte(s)\n", intSize);
+	printf("Size of a long int: %i byte(s)\n", longSize);
+	printf("Size of a long long int: %i byte(s)\n", longLongSize);
+	printf("Size of a float: %i byte(s)\n", floatSize);
 
 	return (0);
 
