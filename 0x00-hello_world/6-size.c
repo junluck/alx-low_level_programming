011e77e50371bfb2eb06c8b888a65dc253642f99
junluck <junaindavidsvickerman@gmail.com>
2022-09-09 02:57:02 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main-
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+
+	retunrn 0
+
+
+}
