9b649b8a73f16f95d8aee63680b4099f41e634fb
junluck <junaindavidsvickerman@gmail.com>
2022-09-08 17:49:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main-Using printf to output string
+ *
+ * Description:Using printf to output string
+ *
+ *Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
