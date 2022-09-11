b2dd3e36c4a293a7cd17539f2299dc98f71dfde5
junluck <junaindavidsvickerman@gmail.com>
2022-09-11 09:34:14 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{	
+	int alph;
+	for (alph='a';alph<='z';alph++)
+	{
+		
+		putchar(alph);
+	
+	}
+	return (0);
+	
+}
