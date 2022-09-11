c4d02b0d06101b97b12924cbd8c064e7c74bf85a
junluck <junaindavidsvickerman@gmail.com>
2022-09-11 06:09:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,38 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
+
+/*
+ * main - Entry point for the main function
  *
- **main-Entry point
+ * Description: Storing random number in variable and getting remainder
  *
- ** Description:assigning the variable n with the value of a random number and then assigning a new variable called number with the last digit of n.
- Then using a if and else statment to outpout different messages for what the result of the last digit will be
+ * Return: Always 0 (Success)
  *
- **Return: Always 0 (Success)
- *
- **/
+ */
 
 int main(void)
 
 {
 		int n;
+
 		int number;
-		srand ( time ( 0 ) );
+
+		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		number=n % (n / 10);
-		if(number < 6 && number != 0)
+		number = n % (n / 10);
+		if (number < 6 && number != 0)
 		{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n , number);		}
-		else if(number == 0)
+			printf("Last digit of %iis%i and is less than 6 and not 0\n", n, number);		}
+		else if (number == 0)
 		{
-		printf("Last digit of %i is %i and is 0\n" , n , number);
+			printf("Last digit of %i is %i and is 0\n", n, number);
 		}
-		else if(number > 5)
-		{ 
-		 printf("Last digit of %i is %i and is greater than 5\n", n , number);
+		else if (number > 5)
+		{
+			printf("Last digit of %i is %i and is greater than 5\n", n, number)
 		}
+
 		return (0);
 
 }
