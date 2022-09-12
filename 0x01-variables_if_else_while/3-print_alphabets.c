f99264a1e4643c61575b5fac914e9b96205b0190
junluck <junaindavidsvickerman@gmail.com>
2022-09-12 06:41:49 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- **main-Entry point for the main function
+/**
+ *main-Entry point for the main function
  *
  *Description:Printing out alphabet from a to Z
  *
  *Return:Always 0 (Success)
- **/
+ */
 int main(void)
 {
 	int alph;
+	int alphU;
 	int space = 10;
 
-	for (alph = 'a' ; alph <= 'Z' ; alph++)
+	for (alph = 'a' ; alph <= 'z' ; alph++)
 	{
 		putchar (alph);
+	}
+	for (alphU = 'A' ; alphU <= 'Z'; alphU++)
+	{
+		putchar (alphU);
 	}
 	putchar (space);
 
