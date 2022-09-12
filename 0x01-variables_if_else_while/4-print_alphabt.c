f944c58fe74fe9321ad45a042e0e71de5dabe515
junluck <junaindavidsvickerman@gmail.com>
2022-09-12 09:56:49 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-Entry point for the main function
+ *
+ *Description:printing all letters but e and q
+ *
+ *Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int alp;
+	int space = 10;
+
+	for (alp = 'a' ; alp < 'z' ; alp++)
+	{
+		if (alp == 'e' || alp == 'q')
+		{
+			alp++;
+		};
+		putchar (alp);
+	}
+
+	putchar (space);
+
+	return (0);
+}
