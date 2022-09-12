0393dd90481b092222895a28d6cafcb81f223774
junluck <junaindavidsvickerman@gmail.com>
2022-09-12 10:22:19 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-Entry point for the main function
+ *
+ * Description:printing out the base of 10
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%i", n);
+	}
+	printf("\n");
+	return (0);
+
+}
