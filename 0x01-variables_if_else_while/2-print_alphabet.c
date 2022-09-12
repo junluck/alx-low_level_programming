cbd49ac94a6ae0335f424dc91c460386735c92a2
junluck <junaindavidsvickerman@gmail.com>
2022-09-12 05:20:32 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
+ *main-Entry point for the main function
  *
+ *Description:Printing out alphabet from a to z
  *
- *
- *
+ *Return:Always 0 (Success)
  */
+
 int main(void)
-{	
+{
 	int alph;
-	int space=10;
-	for (alph='a';alph<='z';alph++)
+	int space = 10;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
 	{
-		
-		putchar(alph);
-	
+
+		putchar (alph);
+
 	}
-	putchar(space);
+	putchar (space);
 	return (0);
-	
+
 }
