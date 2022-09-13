d2edf30db64337cb9e7e8183a8797c90e7026ab2
junluck <junaindavidsvickerman@gmail.com>
2022-09-13 10:54:47 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_islower-Entry point for the _islower function
+ *
+ *@c:parameter which is an interger
+ *
+ *Description:using a function to check if c a lower/uppercase alphabet
+ *
+ *Return:Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int n;
+	int m;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		if (c == n)
+		{
+			return (1);
+		}
+
+	}
+
+	for (m = 'A' ; m <= 'Z' ; m++)
+	{
+		if (c == m)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
