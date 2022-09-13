a64a4182f8e3c79afc48baaf3a0b4b140166a024
junluck <junaindavidsvickerman@gmail.com>
2022-09-13 10:02:34 -0700

    int _islower(int c)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *int _islower-Entry point for the int _islower function
+ *
+ *Description:using a function to check if its input is alphabey
+ *
+ *Return:Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int n;
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		if(c == n)
+		{
+			printf("1");
+		}
+
+		else {printf("0");}
+	}
+}
