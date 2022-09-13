ddca77d00fad6f9b967aac6adee3c1f60478a7e7
junluck <junaindavidsvickerman@gmail.com>
2022-09-13 10:31:35 -0700

    int _islower(int c);

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *int _islower-Entry point for the int _islower function
+ *_islower-Entry point for the _islower function
  *
- *Description:using a function to check if its input is alphabey
+ *Description:using a function to check if itsa lowercase alphabet
  *
  *Return:Always 0 (Success)
  */
 int _islower(int c)
 {
 	int n;
+
 	for (n = 'a' ; n <= 'z' ; n++)
 	{
-		if(c == n)
+		if (c == n)
 		{
 			return (1);
 		}
 
-		
+
 
 	}
 
