1e063e36d5387b5c2515551b3f8ce63e02834f8f
junluck <junaindavidsvickerman@gmail.com>
2022-09-13 05:09:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-Entry point for the main function
+ *
+ *Description:print out _putchar
+ *
+ *Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int under = '_';
+	int p = 'p';
+	int u = 'u';
+	int t = 't';
+	int c = 'c';
+	int h = 'h';
+	int a = 'a';
+	int r = 'r';
+	int lineBr = 10;
+	putchar(under);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar(lineBr);
+
+	return (0);
+}
