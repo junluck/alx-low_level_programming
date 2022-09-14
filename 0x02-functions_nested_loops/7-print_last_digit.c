f63d4d66dc45f205b46923a965d445d849adf01f
junluck <junaindavidsvickerman@gmail.com>
2022-09-14 13:04:51 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_last_digit-Entry point for the print_sign function
+ *
+ *@n:parameter which is an interger
+ *
+ *Description:using a function to get a remainder
+ *
+ *Return:Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+
+	int rem = n / 10;
+	int result = n % rem;
+
+	return (result);
+}
