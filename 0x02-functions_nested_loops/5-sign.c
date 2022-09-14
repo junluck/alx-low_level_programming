153458fafbb98383ae4b219931b7b3383f268c07
junluck <junaindavidsvickerman@gmail.com>
2022-09-13 23:34:11 -0700

    int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *print_sign-Entry point for the print_sign function
+ *
+ *@n:parameter which is an interger
+ *
+ *Description:using a function to to see if n is pos/neg/equaltozero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+,");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0,");
+		return (0);
+	}
+	else if (n < 0)
+	{
+		printf("-,");
+		return (-1);
+	}
+}
