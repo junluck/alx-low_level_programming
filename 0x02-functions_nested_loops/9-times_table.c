494c4573ccb1733d8dfb95bbd7cb512fe8f27135
junluck <junaindavidsvickerman@gmail.com>
2022-09-14 14:31:08 -0700

    void times_table(void);

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *times_table-Entry point for the print_sign function
+ *
+ *Description:using a function to print out nested loop
+ *
+ *Return:Always 0 (Success)
+ */
+void times_table(void);
+{
+
+	int n;
+	int m;
+	int o;
+	int linebr = 10;
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		for (m = 0 ; m <= 9 ; m++)
+		{
+			int ans = n + ans;
+
+			return(ans);
+		}
+		putchar(linebr);
+
+	}
+
+	return (result);
+}
