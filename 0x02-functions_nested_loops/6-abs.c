4e4c43514ae54ffc4aa57aaaaef2c204a5506bfa
junluck <junaindavidsvickerman@gmail.com>
2022-09-14 12:24:15 -0700

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *_abs-Entry point for the print_sign function
+ *
+ *@int:parameter which is an interger
+ *
+ *Description:using a function to get abosulte value
+ */
+int _abs(int)
+{
+
+	if (int >= 0)
+	{
+		int ab = int * 1;
+		return (qb);
+		 
+	}
+	else if (int < 0)
+	{	
+		int abs = int * -1;
+		return (abs);
+		
+	};
+
+	return (0);
+}
