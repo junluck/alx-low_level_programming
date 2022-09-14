604414e7b3c5a2011be23f926e0b953d9c82c1c1
junluck <junaindavidsvickerman@gmail.com>
2022-09-14 12:41:39 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,27 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
  *_abs-Entry point for the print_sign function
  *
- *@int:parameter which is an interger
+ *@n:parameter which is an interger
  *
  *Description:using a function to get abosulte value
  */
 int _abs(int n)
 {
-	
+
 	if (n >= 0)
 	{
 		int ab = n * 1;
+
 		return (ab);
-		 
+
 	}
 	else if (n < 0)
-	{	
+	{
 		int abs = n * -1;
+
 		return (abs);
-		
+
 	};
 
 	return (0);
