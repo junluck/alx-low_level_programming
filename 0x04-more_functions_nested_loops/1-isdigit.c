ebd8d3a3286a2119926ebd7fdd928593132e2a30
junluck <junaindavidsvickerman@gmail.com>
2022-09-15 14:35:25 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_isdigit-Entry point for the _isdigit function
+ *
+ *@c:a parameter for an int
+ *
+ *Description:checking if character is number
+ *
+ *Return:Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
