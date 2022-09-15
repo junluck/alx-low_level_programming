5beb3206377ce8aececc88926f8762295add761a
junluck <junaindavidsvickerman@gmail.com>
2022-09-15 15:26:48 -0700

    int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *mul-Entry point for the _isdigit function
+ *
+ *@a:a parameter for an int
+ *@b: a parameter for an int 
+ *
+ *Description:mutiplying two integers
+ *
+ *Return:Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int answer = a * b;
+
+	return (answer);
+}
