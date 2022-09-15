5420dc1436c76a905f1295436499774d65d8ad45
junluck <junaindavidsvickerman@gmail.com>
2022-09-15 15:55:28 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_numbers-Entry point for the _isdigit function
+ *
+ *Description:printing numbers from 0 to 9
+ *
+ *Return:Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 48 ; i <= 57 ; i++)
+	{
+		putchar(count)
+
+	}
+}
