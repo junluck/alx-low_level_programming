80f8e3826ef1f032c97ad8d84fd4065da3efde19
junluck <junaindavidsvickerman@gmail.com>
2022-09-16 12:34:05 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *more_numbers-Entry point for the more_numbers function
+ *
+ *Description:printing numbers from 0 to 14
+ *
+ *Return:Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int count;
+	int lineBr = 10;
+	int countTwo;
+
+	for (count = 0 ; count < 14 ; count++)
+	{
+		for (countTwo = 48 ; countTwo < 57 ; countTwo++)
+		{
+			_putchar();
+		}
+
+	}
+	putchar(lineBr);
+}
