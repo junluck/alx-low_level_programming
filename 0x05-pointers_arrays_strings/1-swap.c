7308d3a8e0306306b713c6927eeed51906c2b0bd
junluck <junaindavidsvickerman@gmail.com>
2022-09-19 04:13:55 -0700

    void swap_int(int *a, int *b);

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *swap_int-Entry point for the swap_int function
+ *
+ *@a:The int thats going to swap with b
+ *
+ *@b:The int thats going to swap with a
+ *
+ *Description:assigning 98 to a pointer
+ *
+ *Return:Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
