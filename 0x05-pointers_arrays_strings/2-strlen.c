98dd4598b3ddb4dd2616ad2a229a7da6ca4dcf25
junluck <junaindavidsvickerman@gmail.com>
2022-09-19 06:18:51 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strlen-Entry point for the _strlen function
+ *
+ *@s:get the length of this string
+ *
+ *Description:get the length of the string
+ *
+ *Return:Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int sum = 0;
+	int i = 0;
+	char c = s[0];
+
+	while (c != '\0')
+	{
+		sum++;
+		c = s[i++];
+	}
+	return (sum);
+}
