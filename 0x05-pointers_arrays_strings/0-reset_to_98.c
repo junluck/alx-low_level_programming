1f785f7be18ce365b014143736171d7dbe942139
junluck <junaindavidsvickerman@gmail.com>
2022-09-19 03:07:28 -0700

    void reset_to_98(int *n);

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *reset_to_98-Entry point for the reset_to_98 function
+ *
+ *@n:The int thats going to change
+ *
+ *Description:assigning 98 to a pointer
+ *
+ *Return:Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
