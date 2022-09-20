a2c7abf26068ee2b4fd30463d4256c052d147a8d
junluck <junaindavidsvickerman@gmail.com>
2022-09-20 11:28:08 -0700

    "%i, ",a[i]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,11 @@ void print_array(int *a, int n)
 	int i;
 	int lineBr = 10;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n - 1 ; i++)
 	{
 		printf("%i, ",a[i]);
 	}
+	printf("%i",a[n]);
 	putchar(lineBr);
 
 }
