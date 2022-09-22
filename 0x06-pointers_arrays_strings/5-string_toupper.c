a34c6c9d99bef8ff0d824bd4dafe4fcd4cb5a72d
junluck <junaindavidsvickerman@gmail.com>
2022-09-22 11:48:14 -0700

    char *string_toupper(char *)

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *string_toupper-Entry point for the reverse_array function
+ *
+ *@*:the pointer that must be uppercase
+ *
+ *Description:Convert string to uppercase
+ *
+ *Return:Always 0 (Success)
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+	int k;
+	int sum = -1;
+	char c = a[0];
+
+	while (c != '\0')
+	{
+		sum++;
+		c = a[i++];
+	}
+
+	for (k = 0 ; k <= sum ; k++)
+	{
+		if (a[k] >= 97 && a[k] <= 122)
+		{
+			a[k] + 42;
+		}
+	}
+}
