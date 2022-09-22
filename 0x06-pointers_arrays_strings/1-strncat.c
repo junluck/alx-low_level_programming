4085235e1f34dee7082f65ce0cf21ff1279b1ca4
junluck <junaindavidsvickerman@gmail.com>
2022-09-22 07:45:18 -0700

    char *_strncat(char *dest, char *src, int n);

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **_strncat-Entry point for the *_strncat function
+ *
+ *@dest:array of chars
+ *
+ *@src:second array of chars
+ *
+ *@n:number of chars
+ *
+ *Description:concatenates two strings
+ *
+ *Return:Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+	int sum = -1;
+	int sumTwo = 0;
+	char c = dest[0];
+	char d = src[0];
+
+	while (c != '\0')
+	{
+		sum++;
+		c = dest[i++];
+	}
+	while (d != '\0')
+	{
+		sumTwo++;
+		d = src[j++];
+	}
+	for (k = 0 ; k < n ; k++)
+	{
+		dest[sum + k] = src[k];
+	}
+
+	return (dest);
+}
