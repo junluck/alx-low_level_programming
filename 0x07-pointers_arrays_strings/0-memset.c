1adea6fb57fea07092e655f86e446e81e42ee983
junluck <junaindavidsvickerman@gmail.com>
2022-09-26 06:57:48 -0700

    memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **_memset-Entry point for the *_memset function
+ *
+ *@*s:pointer to chars
+ *
+ *@b:chars
+ *
+ *@n:unsigned int
+ *
+ *Description:fills memory with a constant byte
+ *
+ *Return:Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{	
+	int i;
+	int total = sizeof(char) * n;
+	for (i = 0 ; i <= total ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
