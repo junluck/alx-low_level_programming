4c59a6120d1de2046bb4a276c88ab141cc03817e
junluck <junaindavidsvickerman@gmail.com>
2022-09-27 06:25:52 -0700

    &s[i]

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **_strpbrk-Entry point for the *_strpbrk function
+ *
+ *@s:pointer to chars
+ *
+ *@accept:chars to look for
+ *
+ *Description: searches a string for any of a set of bytes.
+ *
+ *Return:Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int total = sizeof(s);
+	char *ptr1 = NULL;
+
+	unsigned int counter = 0;
+
+	for (i = 0 ; i <= total ; i++)
+	{
+
+		if (&accept[i] == &s[i])
+		{
+				ptr1 = &s[i];
+		}
+	}
+	return (ptr1);
+}
