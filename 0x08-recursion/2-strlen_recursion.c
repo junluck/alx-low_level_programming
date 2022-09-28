887fd29c3173047df54cfe7ec87273281fcc964b
junluck <junaindavidsvickerman@gmail.com>
2022-09-28 06:01:14 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strlen_recursion-Entry point for the _strlen_recursion function
+ *
+ *@s:pointer to chars
+ *
+ *Description:returns number of characters
+ *
+ *Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{	int counter;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		counter = sizeof(*s + 1);
+	}
+
+	return (counter);
+}
