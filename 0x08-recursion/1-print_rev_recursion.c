2236d624a5d511ba49a8a019819e62fa42919407
junluck <junaindavidsvickerman@gmail.com>
2022-09-28 03:16:14 -0700

    0-puts_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,21 +3,22 @@
 /**
  *_puts_recursion-Entry point for the _puts_recursion function
  *
- *@s:pointer to chars
+ *@s-pointer to chars
  *
- *Description:prints out string using recursion.
+ *Description:prints a string, followed by a new line.
+ *
+ *Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
 
 	if (*s != '\0')
 	{
-		putchar(*s);
 		_puts_recursion(s + 1);
+		putchar(*s);
 	};
 	if (*s == '\0')
 	{
 		putchar(10);
-	};
-
+	}
 }
