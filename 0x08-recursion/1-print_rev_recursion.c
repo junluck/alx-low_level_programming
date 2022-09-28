78f45e4514b3632baa12cdfe8b3b6f246a480c80
junluck <junaindavidsvickerman@gmail.com>
2022-09-28 03:29:10 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,14 +11,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-
+	if(*s == '\0')
+	{
+		putchar(10)
+	};
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		putchar(*s);
-	};
-	if (*s == '\0')
-	{
-		putchar(10);
-	}
+	}:
 }
