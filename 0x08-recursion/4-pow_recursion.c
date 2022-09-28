1396cb3ee5df6f36c9bf790b7a5d5780da810d6b
junluck <junaindavidsvickerman@gmail.com>
2022-09-28 08:56:37 -0700

    _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_pow_recursion-Entry point for the _pow_recursion function
+ *
+ *@x:The int
+ *
+ *@y:The int
+ *
+ *Description:getting power of number
+ *
+ *Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		_pow_recursion(x,y - 1);
+		return (n * n);
+	}
+
+	return (-1);
+}
