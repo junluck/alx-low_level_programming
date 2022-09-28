c45707cc467379783bc7124996c6ae66cd20aacc
junluck <junaindavidsvickerman@gmail.com>
2022-09-28 08:29:24 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *factorial-Entry point for the factorial function
+ *
+ *@n:The int
+ *
+ *Description:getting factorial of number
+ *
+ *Return: Always 0.
+ */
+int factorial(int n)
+{	
+	if (n > 0)
+	{
+		return (n * (factorial(n-1)));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (-1);
+}
