469f9945c829b714125eeecf7b18ff195b5fc403
junluck <junaindavidsvickerman@gmail.com>
2022-09-29 06:52:07 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -21,9 +21,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1) * -1);
 	}
-	else if (y < 0 )
+	else if (y < 0)
 	{
-		return (- 1);
+		return (-1);
 	};
 
 	return (1);
