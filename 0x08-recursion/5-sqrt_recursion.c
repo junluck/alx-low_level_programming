4400381a5313d90432e6dcc4da439ea24e7e2888
junluck <junaindavidsvickerman@gmail.com>
2022-09-29 13:01:54 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_sqrt_recursion-Entry point for the _sqrt_recursion function
+ *
+ *@n:The int
+ *
+ *Description:returns the natural square root of a number
+ *
+ *Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if()
+
+	return (1);
+}
