380d21d3f0595c4233b02a30c5d8ebb42554a233
junluck <junaindavidsvickerman@gmail.com>
2022-09-30 05:32:44 -0700

    nm libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _isupper(int c);
+/**
+ *_isupper-Entry point for the _isalpha functio
+ *
+ *@c:a parameter for an int
+ *
+ *Description:checking if character is upper case
+ *
+ *Return:Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 65 ; i <= 90 ; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
