d37d5fa3fb9f8311462d710b4f8c8e14cd2258db
junluck <junaindavidsvickerman@gmail.com>
2022-09-30 02:25:23 -0700

    ./args You can do anything, but not everything.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-entry point for the main function
+ *
+ *@argv:argument vector
+ *
+ *@argc:argument count
+ *
+ *Description:prints program name
+ *
+ *Return:Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s", argv[i]);
+		putchar(10);
+	}
+
+	return (0);
+}
