13430487bf0e4b67bbb1a871815b3b8cdeb0cb25
junluck <junaindavidsvickerman@gmail.com>
2022-10-04 12:26:18 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strdup.c -o s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **_strdup-Entry point for the _strdup function
+ *
+ *@str:pointer to string
+ *
+ *Description:duplicate string
+ *
+ *Return:Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	int i;
+	int sizer = sizeof(str);
+
+	if (str != NULL)
+	{
+		ar = malloc(sizeof(str));
+		for (i = 0 ; i <= sizer; i++)
+		{
+			ar[i] = str[i];
+		}
+	}
+	else if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (ar);
+}
