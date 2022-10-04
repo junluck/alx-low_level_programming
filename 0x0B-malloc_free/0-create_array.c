f85c1b3b07f00203bbff7d067ffa177ec45ac5e0
junluck <junaindavidsvickerman@gmail.com>
2022-10-04 10:54:22 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,21 +3,26 @@
 /**
  **create_array-Entry point for the create_array function
  *
- *@size: size of the array 
+ *@size: size of the array
  *
- *@c:The char 
+ *@c:The char
  *
  *Description:creating a array with malloc
  *
  *Return:Always 0 (Success)
  */
 char *create_array(unsigned int size, char c)
-{	
+{
 	char *ar;
+	unsigned int i;
 
 	if (size > 0)
 	{
 		ar = malloc(sizeof(c) * size);
+		for (i = 0 ; i < size ; i++)
+		{
+			ar[i] = c;
+		}
 	}
 	else if (size == 0)
 	{
