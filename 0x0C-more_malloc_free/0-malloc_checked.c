5217e6701c280cb3e471f8473e678f327c5fbff9
junluck <junaindavidsvickerman@gmail.com>
2022-10-06 07:24:50 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **malloc_checked-Entry point for *malloc_checked function
+ *
+ *@b:unsigned int
+ *
+ *Description:allocate memory with malloc
+ *
+ *Return::Always 0 (Success)
+ */
+void *malloc_checked(unsigned int b)
+{
+	
+	int *ptr1 = malloc(sizeof(b));
+	if (ptr1 == NULL)
+	{
+		exit(3);
+	}
+
+}
