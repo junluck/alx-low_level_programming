536a268d596f51d139e7ebab1aa5cf4bb36d124e
junluck <junaindavidsvickerman@gmail.com>
2022-10-10 11:43:08 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,24 @@
-#include <stdio.h>
 #ifndef DOG
 #define DOG
-
-struct dog {
-
-	char * name;
+#include <stdio.h>
+/**
+ *struct dog-Entry point for the struct dog
+ *
+ *@name:name of dog
+ *
+ *@age:age of dog
+ *
+ *@owner:owner of dog
+ *
+ *Description:Define a new type struct dog with the following elements
+ *
+ *Return:Always 0 (Success)
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 
 };
-
-#endif
+#endif;
