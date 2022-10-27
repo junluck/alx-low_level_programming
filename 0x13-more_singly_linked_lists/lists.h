b75b92ea01e8e9cf895b66b6cee91828d09e6848
junluck <junaindavidsvickerman@gmail.com>
2022-10-27 08:41:58 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_listint.c -o a

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,2 +1,13 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+
+} listint_t;
 size_t print_listint(const listint_t *h);
+
+
