bc0a762e30177ffbda499166a055508cda1dd0ba
junluck <junaindavidsvickerman@gmail.com>
2022-11-23 08:58:58 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 	int i;
 	int binaryAns;
 	int half;
-	
+
 	if (n == 0)
 	{
 		putchar(48);
@@ -31,7 +31,7 @@ void print_binary(unsigned long int n)
 		}
 		else if (binaryAns == 0)
 		{
-			
+
 			putchar(48);
 
 		}
