a67d057d1e93d9bb2a87deeb0c5f92887b0735f4
junluck <junaindavidsvickerman@gmail.com>
2022-12-19 03:14:19 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-get_bit.c -o c

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -15,25 +15,14 @@
  *     */
 
 int main(void)
-
 {
-
-	    int n;
-
-
-
-	        n = get_bit(1024, 10);
-
-		    printf("%d\n", n);
-
-		        n = get_bit(98, 1);
-
-			    printf("%d\n", n);
-
-			        n = get_bit(1024, 0);
-
-				    printf("%d\n", n);
-
-				        return (0);
+	int n;
+	n = get_bit(1024, 10);
+	printf("%d\n", n);
+	n = get_bit(98, 1);
+	printf("%d\n", n);
+	n = get_bit(1024, 0);
+	printf("%d\n", n);
+	return (0);
 
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,3 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
