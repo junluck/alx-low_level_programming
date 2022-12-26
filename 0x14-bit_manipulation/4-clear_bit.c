7e6072cf18dc697893bedf0a59d07dbd06a10da3
junluck <junaindavidsvickerman@gmail.com>
2022-12-26 04:22:28 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * 4-clear_bit.c -entry point for 4-clear_bit.c function
+ *clear_bit-entry point for clear_bit function
  *
  *@n: long int that needs to be index
  *
  *@index:index number
  *
- *Description:a function that sets the value of a bit to 0 at a given index.
+ *description:a function that sets the value of a bit to 0 at a given index.
  *
  *Return:-1 if error or 1 if worked
  */
@@ -18,7 +18,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int binaryTwo = binary & 1;
 	unsigned long int binaryThree = binaryTwo << index;
 	unsigned long int binaryFour = ans | binaryThree;
+
 	*n = binaryFour;
 
-	return(1);	
+	return (1);
 }
